041021dc6953a398d95e84fb791664e3cdf7fd56
Tim Robinson <tim@amazon.com>
2019-11-26 12:47:38 -0800

    Setup docker file and run colcon build

---
+++ .github/actions/ros1-ci/scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export CATKIN_TEST_COVERAGE=1
+
+ROS_DISTRO=$1
+PACKAGE_PATH=$2
+
+cd $PACKAGE_PATH
+[ -e ".rosinstall" ] && rosws update
+rosdep install --from-paths . --ignore-src --rosdistro "${ROS_DISTRO}" -r -y
+
+. /opt/ros/${ROS_DISTRO}/setup.sh
+
+colcon build
