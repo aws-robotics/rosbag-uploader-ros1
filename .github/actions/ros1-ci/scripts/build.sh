53ee0bd65608823611cb10367b28704a0b6be74e
Tim Robinson <tim@amazon.com>
2019-11-26 13:52:31 -0800

    Move rosdep update to build script

--- .github/actions/ros1-ci/scripts/build.sh
+++ .github/actions/ros1-ci/scripts/build.sh
@@ -5,6 +5,8 @@ export CATKIN_TEST_COVERAGE=1
 ROS_DISTRO=$1
 PACKAGE_PATH=$2
 
+rosdep update
+
 cd $PACKAGE_PATH
 [ -e ".rosinstall" ] && rosws update
 rosdep install --from-paths . --ignore-src --rosdistro "${ROS_DISTRO}" -r -y
