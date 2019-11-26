7ad6ce42a425e042ad0e55bb07d8b0094bd78229
Tim Robinson <tim@amazon.com>
2019-11-25 16:58:54 -0800

    Fix imports

--- .github/actions/ros1-ci/dist/index.js
+++ .github/actions/ros1-ci/dist/index.js
@@ -9480,24 +9480,28 @@ function authenticationBeforeRequest(state, options) {
 
 "use strict";
 
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = __importDefault(__webpack_require__(470));
-const github_1 = __importDefault(__webpack_require__(469));
+const core = __importStar(__webpack_require__(470));
+const github = __importStar(__webpack_require__(469));
 try {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core_1.default.getInput('who-to-greet');
+    const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
-    core_1.default.setOutput("time", time);
+    core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github_1.default.context.payload, undefined, 2);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
 }
 catch (error) {
-    core_1.default.setFailed(error.message);
+    core.setFailed(error.message);
 }
 
 
