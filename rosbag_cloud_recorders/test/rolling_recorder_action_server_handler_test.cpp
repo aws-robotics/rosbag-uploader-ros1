495583a5c082d618878c143fdeb7cd617213c345
Abby Xu <abbyxu@amazon.com>
2020-01-24 10:12:05 -0800

    Add unit tests

--- rosbag_cloud_recorders/test/rolling_recorder_action_server_handler_test.cpp
+++ rosbag_cloud_recorders/test/rolling_recorder_action_server_handler_test.cpp
@@ -21,6 +21,8 @@
 #include <boost/filesystem.hpp>
 #include <ros/ros.h>
 #include <fstream>
+#include <thread>
+#include <std_msgs/String.h>
 #include <rosbag_cloud_recorders/rolling_recorder/rolling_recorder_action_server_handler.h>
 
 using namespace Aws::Rosbag;
@@ -57,12 +59,13 @@ protected:
   std::string write_directory;
   ros::Duration bag_rollover_time;
   boost::filesystem::path path;
+
 public:
   RollingRecorderActionServerHandlerTests():
     goal_handle(std::make_shared<MockRollingRecorderGoalHandle>()),
     goal(new recorder_msgs::RollingRecorderGoal()),
     write_directory(std::string(std::getenv("HOME")) + "/.ros/"),
-    bag_rollover_time(ros::Duration(10, 0)),
+    bag_rollover_time(ros::Duration(30, 0)),
     path(write_directory) {}
 
   void SetUp() override
@@ -97,6 +100,31 @@ public:
   void assertGoalIsAborted() {
     EXPECT_CALL(*goal_handle, setAborted(_, _));
   }
+
+  void createRosbagInWriteDirectory(std::vector<std::string> bag_files) {
+    for(std::string bag_file : bag_files)  {
+
+      std::ofstream new_bag_file(write_directory + bag_file);
+    }
+  }
+
+  void checkGetRosbagsToUploadGetsValidFileExtension(ros::Time time_of_goal_received) {
+    std::vector<std::string>  bag_files_in_write_directory = Aws::Rosbag::RollingRecorderActionServerHandler<MockRollingRecorderGoalHandle>::GetRosbagsToUpload(write_directory, bag_rollover_time, time_of_goal_received);
+    for (std::string bag_file_in_write_directory : bag_files_in_write_directory) {
+      EXPECT_EQ(boost::filesystem::extension(bag_file_in_write_directory), ".bag");
+    }
+  }
+  void constructTestRosbagFile(std::string ros_bag_file_name, ros::Time time_of_message_created) {
+    rosbag::Bag new_ros_bag;
+    new_ros_bag.open(ros_bag_file_name, rosbag::bagmode::Write);
+
+
+    std_msgs::String test_message;
+    test_message.data = std::string("Running unit tests rolling recorder.");
+
+    new_ros_bag.write("rolling_recorder_unit_test", time_of_message_created, test_message);
+    new_ros_bag.close();
+  }
 };
 
 TEST_F(RollingRecorderActionServerHandlerTests, TestRollingRecorderRosbagUpload)
@@ -115,16 +143,35 @@ TEST_F(RollingRecorderActionServerHandlerTests, TestCancelRollingRecorderRosbagU
 
 TEST_F(RollingRecorderActionServerHandlerTests, TestGetRosbagToUpload)
 {
-  // Create a list of files in the write_directory
-  // TODO(abbyxu): add test for .bag.active file
-  std::vector<std::string> expected_bag_file_names{"test1.bag", "test2.bag", "wrong_file_ext.txt"};
-  for(std::string expected_file_name : expected_bag_file_names)  {
-    std::ofstream new_bag_file(write_directory + expected_file_name);
-  }
   ros::Time time_of_function_called(ros::Time::now());
-  std::vector<std::string> bag_files_in_write_directory = Aws::Rosbag::RollingRecorderActionServerHandler<MockRollingRecorderGoalHandle>::GetRosbagsToUpload(write_directory, bag_rollover_time, time_of_function_called);
-  for (std::string bag_file_in_write_directory : bag_files_in_write_directory) {
-    EXPECT_EQ(boost::filesystem::extension(bag_file_in_write_directory), ".bag");
+
+  // All valid bag file extension
+  {
+    std::vector<std::string> bag_files_to_create{"test1.bag", "test2.bag"};
+    createRosbagInWriteDirectory(std::move(bag_files_to_create));
+
+    checkGetRosbagsToUploadGetsValidFileExtension(time_of_function_called);
+  }
+
+  // All invalid bag file extension
+  {
+    std::vector<std::string> bag_files_to_create{"test1.bag.test", "test1.bag.random", "test1.bag.how.about.this.Bag", "text_file.txt"};
+    createRosbagInWriteDirectory(std::move(bag_files_to_create));
+
+    checkGetRosbagsToUploadGetsValidFileExtension(time_of_function_called);
+  }
+
+  // Test inactive file case
+  {
+    std::string test_ros_bag_file_name("test_active.bag");
+    ros::Time time_of_message_created(time_of_function_called - ros::Duration(24*60*60));  // 1 day before the test gets ran
+    std::vector<std::string> bag_files_to_create{test_ros_bag_file_name + ".active"};
+    createRosbagInWriteDirectory(std::move(bag_files_to_create));
+    std::thread process_active_bag_file_thread([&] () { checkGetRosbagsToUploadGetsValidFileExtension(time_of_function_called); } );
+    ros::Duration(3, 0).sleep();
+    ASSERT_TRUE(boost::filesystem::remove(write_directory + test_ros_bag_file_name + ".active"));
+    constructTestRosbagFile(write_directory + test_ros_bag_file_name, time_of_message_created);
+    process_active_bag_file_thread.join();
   }
 }
 
