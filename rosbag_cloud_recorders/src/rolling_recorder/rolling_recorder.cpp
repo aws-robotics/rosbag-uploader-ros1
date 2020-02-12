f20f4c3b718f8de20e90a74eb980ec3ddce1fccc
Abby Xu <abbyxu@amazon.com>
2020-02-12 11:37:43 -0800

    Refactor based on common code refactored in Utils

--- rosbag_cloud_recorders/src/rolling_recorder/rolling_recorder.cpp
+++ rosbag_cloud_recorders/src/rolling_recorder/rolling_recorder.cpp
@@ -35,7 +35,7 @@ namespace Rosbag
 RollingRecorder::RollingRecorder(RollingRecorderOptions rolling_recorder_options) :
   node_handle_("~"),
   action_server_(node_handle_, "RosbagRollingRecord", false),
-  rosbag_uploader_action_client_(std::make_shared<UploadFilesActionSimpleClient>("/s3_file_uploader/UploadFiles", true)),
+  rosbag_uploader_action_client_("/s3_file_uploader/UploadFiles", true),
   rolling_recorder_options_(rolling_recorder_options),
   periodic_file_deleter_([this]()->std::vector<std::string>{return this->GetRosBagsToDelete();}, rolling_recorder_options_.bag_rollover_time.toSec()),
   action_server_busy_(false)
